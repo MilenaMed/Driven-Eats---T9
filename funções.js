69343baeed7e767e5495cb95f488662ea9c9bbb6
MilenaMed <milenamfm@gmail.com>
2022-10-16 23:22:48 -0300

    Criar mensagem e redirecionar
    Criar constante para formação das frases, e redirecionar pra wpp com mensagem pronta do pedido

--- funções.js
+++ funções.js
@@ -38,3 +38,42 @@ function habilitaBotaoRodape (fecharpedido){
       pedidofechado.innerHTML = "Finalizar";
   }
 }
+
+function redirecionar (botaofinalizar){
+  let textoBotao = botaofinalizar.textContent
+  if (textoBotao === 'Finalizar'){
+ 
+    const $pratoselecionado = document.querySelector(".selecionadop");
+    const nomeComidaPrato = $pratoselecionado.querySelector(".titulo-comida").textContent.trim();
+
+    const $bebidaselecionada = document.querySelector(".selecionadob");
+    const nomeBebida = $bebidaselecionada.querySelector(".titulo-comida").textContent.trim();
+
+    const $sobremesaselecionada = document.querySelector(".selecionados");
+    const nomeSobremesa = $sobremesaselecionada.querySelector(".titulo-comida").textContent.trim();
+
+    const $valorfinalp = document.querySelector(".selecionadop");
+    const valorprato = $valorfinalp.querySelector(".valor").textContent;
+    const valorprato1 = valorprato.trim().replace(",",".").split(" ");
+    let nvalorprato1 = Number(valorprato1[1]);
+
+    const $valorfinalb = document.querySelector(".selecionadob");
+    const valorbebida = $valorfinalb.querySelector(".valor").textContent;
+    const valorbebida1 =  valorbebida.trim().replace(",",".").split(" ");
+    let nvalorbebida1 = Number( valorbebida1[1]);
+
+    const $valorfinals = document.querySelector(".selecionados");
+    const valorsobremesa = $valorfinals.querySelector(".valor").textContent;
+    const valorsobremesa1 =  valorsobremesa.trim().replace(",",".").split(" ");
+    let nvalorsobremesa1 = Number( valorsobremesa1[1]);
+
+  let meuTexto = 
+  "Olá, gostaria de fazer o pedido:" +
+  "\n- Prato:"+ nomeComidaPrato + 
+  "\n- Bebida:"+ nomeBebida+ 
+  "\n- Sobremesa:" + nomeSobremesa +
+  "\nTotal: R$" + (nvalorprato1 + nvalorbebida1 + nvalorsobremesa1);
+  
+  window.location.href = `https://wa.me/?text=${encodeURIComponent(meuTexto)}`;
+  }
+}
